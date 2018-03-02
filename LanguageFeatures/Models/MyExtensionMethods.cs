de808aaac423e238b946a8f3abb697db5bd3baf7
Roy Lovitt <lovittl@my.erau.edu>
2018-03-02 06:40:40 -0500

    update MyExtensionMethods to use IEnumerable. #Closes 5

--- LanguageFeatures/Models/MyExtensionMethods.cs
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -9,10 +9,10 @@ namespace LanguageFeatures.Models
 {
     public static class MyExtensionMethods
     {
-        public static decimal TotalPrices(this ShoppingCart cartParam)
+        public static decimal TotalPrices(this IEnumerable<Product> productEnum)
         {
             decimal total = 0;
-            foreach (Product prod in cartParam.Products)
+            foreach (Product prod in productEnum)
             {
                 total += prod.Price;
             }
