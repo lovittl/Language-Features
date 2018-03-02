3ed04d4227f24b0ec708111754849794e7af24c7
Roy Lovitt <lovittl@my.erau.edu>
2018-03-02 06:34:49 -0500

    revise shopping cart to use IEnumerable. #Closes 4

--- LanguageFeatures/Models/ShoppingCart.cs
+++ LanguageFeatures/Models/ShoppingCart.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using LanguageFeatures.Models;
-//using LanguageFeatures.Controllers;
+using System.Collections;
 
 namespace LanguageFeatures.Models
 {
-    public class ShoppingCart
+    public class ShoppingCart : IEnumerable<Product>
     {
         public List<Product> Products { get; set; }
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
